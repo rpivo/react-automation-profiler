cb67745be804d233fc2b8cd485c9ad4dabd0fe7d
Ryan <ryanpivovar@gmail.com>
2020-10-16 18:12:56 -0400

    remove lib from git

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-declare const _default: {
-    AutomationProfiler: import("react").FC<{
-        children: import("react").ReactNode;
-        id: string;
-    }>;
-};
-export default _default;

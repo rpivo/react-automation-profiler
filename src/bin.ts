7f7a3d348aa3d39e153738687ccc4f831aba50b9
Ryan <ryanpivovar@gmail.com>
2020-10-17 10:33:16 -0400

    start express server on bin

--- src/bin.ts
+++ src/bin.ts
@@ -1,17 +1,50 @@
 #!/usr/bin/env node
 import { exec } from 'child_process';
+import express from 'express';
+import yargs from 'yargs';
 
-const url = 'http://localhost';
+(async () => {
+  const options = yargs
+    .usage('Usage: -p <port>')
+    .option('p', {
+      alias: 'port',
+      describe: 'port to be used for server',
+      type: 'number',
+    })
+    .argv;
 
-const startCommand = () => {
-  switch (process.platform) {
-    case 'darwin':
-      return 'open';
-    case 'win32':
-      return 'start';
-    default:
-      return 'xdg-open';
-  }
-};
+  const {
+    port = 1235,
+  } = options;
 
-exec(`${startCommand()} ${url}`);
+  const url = `http://localhost:${port}`;
+
+  const openPage = () => {
+    const startCommand = () => {
+      switch (process.platform) {
+        case 'darwin':
+          return 'open';
+        case 'win32':
+          return 'start';
+        default:
+          return 'xdg-open';
+      }
+    };
+
+    exec(`${startCommand()} ${url}`);
+  };
+
+  const startServer = async () => {
+    const app = express();
+
+    app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
+
+    app.listen(port, () => {
+      console.log(`Example app listening at ${url}`);
+    })
+  };
+
+  await startServer().then(openPage);
+})();

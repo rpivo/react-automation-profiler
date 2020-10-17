7e8162eddc091c0ad0294c4f65d6013158bc5c0d
Ryan <ryanpivovar@gmail.com>
2020-10-17 00:06:10 -0400

    update bin.ts to be an es module

--- src/bin.ts
+++ src/bin.ts
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
-const { exec } = require('child_process');
+import { exec } from 'child_process';
 
-const copyFiles = exec('sh copyFiles.sh',
-  (error: Error, stdout: string, stderr: string) => {
+exec('sh copyFiles.sh',
+  (error, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     if (error !== null) {
       console.log(`exec error: ${error}`);
     }
   });
-
-  copyFiles();

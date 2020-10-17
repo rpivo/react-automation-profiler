11f305f49d4c549e65ca31ee9ac3b78b790c4c49
Ryan <ryanpivovar@gmail.com>
2020-10-17 13:57:08 -0400

    add back index.d.ts to lib

--- sh/build.sh
+++ sh/build.sh
@@ -1,5 +1,5 @@
 rm -rf lib;
 mkdir lib;
 tsc --declaration;
-rm lib/automation.d.ts && rm lib/bin.d.ts && rm lib/index.d.ts;
+rm lib/automation.d.ts && rm lib/bin.d.ts;
 cp src/index.html lib/index.html;

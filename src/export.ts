5cb707dce79ec0d02202961d6b63df0a4e58e7cd
Ryan <ryanpivovar@gmail.com>
2020-11-04 23:12:21 -0500

    refactor export.ts to use fs/promises

--- src/export.ts
+++ src/export.ts
@@ -8,18 +8,21 @@ import { fileURLToPath } from 'url';
   const scriptPath = fileURLToPath(import.meta.url);
   const path = `${scriptPath.slice(0, scriptPath.lastIndexOf('/'))}`;
   const { JSDOM } = jsdom;
-  const contents = await fs.readFileSync(`${path}/index.html`, 'utf8');
-  const { document } = new JSDOM(`${contents}`).window;
+
+  try {
+    const contents = await fs.readFile(`${path}/index.html`, 'utf8');
+    const { document } = new JSDOM(`${contents}`).window;
+    
+    const chartScriptContents = await fs.readFile(`${path}/charts.js`, 'utf8');
+    const chartScript = document.createElement('script');
+    chartScript.id = 'charts';
+    chartScript.innerHTML = chartScriptContents;
+    chartScript.type = 'module';
   
-  const chartScriptContents = await fs.readFileSync(`${path}/charts.js`, 'utf8');
-  const chartScript = document.createElement('script');
-  chartScript.id = 'charts';
-  chartScript.innerHTML = chartScriptContents;
-  chartScript.type = 'module';
-
-  document.body.appendChild(chartScript);
-
-  await fs.writeFile(`${path}/index.html`, document.documentElement.outerHTML,  err => {
-    if (err) throw err;
-  });
+    document.body.appendChild(chartScript);
+  
+    await fs.writeFile(`${path}/index.html`, document.documentElement.outerHTML);
+  } catch(e) {
+    console.log('❌ An error occurred while appending charts.js to index.html.', e);
+  }
 })();

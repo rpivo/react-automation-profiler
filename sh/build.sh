f2df91aefe6cce5a3bf9a5648370958783a0da6b
Ryan <ryanpivovar@gmail.com>
2021-10-03 18:41:16 -0400

    clean up sh files

--- sh/build.sh
+++ sh/build.sh
@@ -1,10 +1,8 @@
-rm -rf lib;
-mkdir lib;
-tsc;
-rollup -c;
-cp src/index.html lib/index.html;
-cp -R src/types/. lib/;
-node lib/export.js;
-rm lib/charts.js;
-rm lib/charts.util.js;
-rm lib/export.js;
+rm -rf lib
+mkdir lib
+tsc
+rollup -c
+cp src/index.html lib/index.html
+cp -R src/types/. lib/
+node lib/export.js
+rm lib/charts.js lib/charts.util.js lib/export.js
--- sh/diff.sh
+++ sh/diff.sh
@@ -1,4 +1,4 @@
-npm run build;
-echo "current published version:";
-npm show react-automation-profiler version;
-npx publish-diff --filter='{lib,src}/**';
+npm run build
+echo "current published version:"
+npm show react-automation-profiler version
+npx publish-diff --filter='{lib,src}/**'
